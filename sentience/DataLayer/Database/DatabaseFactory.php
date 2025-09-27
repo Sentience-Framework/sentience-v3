8dcd30cadde58239ad170247a3ffce5b9724c99e
UniForceMusic <51342446+UniForceMusic@users.noreply.github.com>
2025-09-27 11:50:29 +0200

    Add database factory

---
+++ sentience/DataLayer/Database/DatabaseFactory.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Sentience\DataLayer\Database;
+
+use Closure;
+use Sentience\Database\Adapters\AdapterAbstract;
+use Sentience\Database\Adapters\SQLite3Adapter;
+use Sentience\Database\Driver;
+
+class DatabaseFactory
+{
+    public static function createMySQL(
+        string $name,
+        string $host = 'localhost',
+        int $port = 3306,
+        string $username = 'root',
+        string $password = '',
+        string $charset = 'utf8mb4',
+        string $engine = 'InnoDB',
+        array $queries = [],
+        ?Closure $debug = null,
+        bool $usePDOAdapter = false
+    ): Database {
+        return new Database(
+            Driver::MYSQL,
+            $host,
+            $port,
+            $name,
+            $username,
+            $password,
+            $queries,
+            [
+                AdapterAbstract::OPTIONS_MYSQL_CHARSET => $charset,
+                AdapterAbstract::OPTIONS_MYSQL_ENGINE => $engine
+            ],
+            $debug,
+            $usePDOAdapter
+        );
+    }
+
+    public static function createPgSQL(
+        string $name,
+        string $host = 'localhost',
+        int $port = 5432,
+        string $username = 'postgres',
+        string $password = '',
+        string $clientEncoding = 'UTF8',
+        array $queries = [],
+        ?Closure $debug = null,
+        bool $usePDOAdapter = false
+    ): Database {
+        return new Database(
+            Driver::PGSQL,
+            $host,
+            $port,
+            $name,
+            $username,
+            $password,
+            $queries,
+            [
+                AdapterAbstract::OPTIONS_PGSQL_CLIENT_ENCODING => $clientEncoding,
+            ],
+            $debug,
+            $usePDOAdapter
+        );
+    }
+
+    public static function createSQLite(
+        string $file,
+        string $encoding = 'UTF8',
+        string $journalMode = 'WAL',
+        string $foreignKeys = true,
+        string $readOnly = false,
+        string $encryptionKey = '',
+        int $busyTimeout = 100,
+        array $queries = [],
+        ?Closure $debug = null,
+        bool $usePDOAdapter = true
+    ): Database {
+        return new Database(
+            Driver::SQLITE,
+            '',
+            0,
+            $file,
+            '',
+            '',
+            $queries,
+            [
+                AdapterAbstract::OPTIONS_SQLITE_ENCODING => $encoding,
+                AdapterAbstract::OPTIONS_SQLITE_JOURNAL_MODE => $journalMode,
+                AdapterAbstract::OPTIONS_SQLITE_FOREIGN_KEYS => $foreignKeys,
+                SQLite3Adapter::OPTIONS_READ_ONLY => $readOnly,
+                SQLite3Adapter::OPTIONS_ENCRYPTION_KEY => $encryptionKey,
+                SQLite3Adapter::OPTIONS_BUSY_TIMEOUT => $busyTimeout,
+            ],
+            $debug,
+            $usePDOAdapter
+        );
+    }
+}
