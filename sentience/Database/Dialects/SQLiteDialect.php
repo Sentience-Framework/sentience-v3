5ecc859b4c393099c754a04041f7acc2132a91b1
UniForceMusic <51342446+UniForceMusic@users.noreply.github.com>
2025-11-25 00:45:23 +0100

    Update SQLiteDialect.php

--- sentience/Database/Dialects/SQLiteDialect.php
+++ sentience/Database/Dialects/SQLiteDialect.php
@@ -49,6 +49,10 @@ class SQLiteDialect extends SQLDialect
 
     protected function buildOnConflict(string &$query, array &$params, ?OnConflict $onConflict, array $values, ?string $lastInsertId): void
     {
+        if (!$this->onConflict()) {
+            return;
+        }
+
         if (is_null($onConflict)) {
             return;
         }
