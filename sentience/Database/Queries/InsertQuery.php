a75637e2bb0a9b47cd15f70c26c44bb7b40e67b8
UniForceMusic <51342446+UniForceMusic@users.noreply.github.com>
2025-11-24 21:31:17 +0100

    Fix insert query

--- sentience/Database/Queries/InsertQuery.php
+++ sentience/Database/Queries/InsertQuery.php
@@ -121,26 +121,26 @@ class InsertQuery extends Query
 
     protected function update(array $conflict, bool $emulatePrepare): ResultInterface
     {
-        $updateQuery = $this->database->update($this->table);
+        if (!is_null($this->onConflict->updates)) {
+            $updateQuery = $this->database->update($this->table);
 
-        $updates = is_null($this->onConflict->updates)
-            ? !empty($onConflict->updates) ? $this->onConflict->updates : $this->values
-            : $conflict;
+            $updates = count($this->onConflict->updates) > 0 ? $this->onConflict->updates : $this->values;
 
-        $updateQuery->values($updates);
+            $updateQuery->values($updates);
 
-        foreach ($conflict as $column => $value) {
-            $updateQuery->whereEquals($column, $value);
-        }
+            foreach ($conflict as $column => $value) {
+                $updateQuery->whereEquals($column, $value);
+            }
 
-        if (!is_null($this->returning)) {
-            $updateQuery->returning($this->returning);
-        }
+            if (!is_null($this->returning)) {
+                $updateQuery->returning($this->returning);
+            }
 
-        $result = $updateQuery->execute($emulatePrepare);
+            $result = $updateQuery->execute($emulatePrepare);
 
-        if (is_null($this->returning) || $this->dialect->returning()) {
-            return $result;
+            if (is_null($this->returning) || $this->dialect->returning()) {
+                return $result;
+            }
         }
 
         return $this->select(
