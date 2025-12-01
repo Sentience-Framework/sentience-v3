4f643c1b1742f0d193fe9b07fb88dd334cf08894
UniForceMusic <51342446+UniForceMusic@users.noreply.github.com>
2025-12-01 10:50:48 +0100

    Add native booleans

--- sentience/Database/Dialects/SQLDialect.php
+++ sentience/Database/Dialects/SQLDialect.php
@@ -37,6 +37,7 @@ class SQLDialect extends DialectAbstract
     protected const bool ESCAPE_ANSI = true;
     protected const string ESCAPE_IDENTIFIER = '"';
     protected const string ESCAPE_STRING = "'";
+    protected const bool BOOL = false;
     protected const bool GENERATED_BY_DEFAULT_AS_IDENTITY = true;
     protected const bool ON_CONFLICT = false;
     protected const bool RETURNING = false;
@@ -968,20 +969,24 @@ class SQLDialect extends DialectAbstract
 
     public function castToQuery(mixed $value): mixed
     {
+        if (is_null($value)) {
+            return 'NULL';
+        }
+
         if (is_string($value)) {
             return $this->escapeString($value);
         }
 
         if (is_bool($value)) {
+            if ($this->bool()) {
+                return $value ? 'TRUE' : 'FALSE';
+            }
+
             $bool = $this->castBool($value);
 
             return is_string($bool)
                 ? $this->escapeString($bool)
                 : $bool;
-        }
-
-        if (is_null($value)) {
-            return 'NULL';
         }
 
         if ($value instanceof DateTimeInterface) {
@@ -993,7 +998,9 @@ class SQLDialect extends DialectAbstract
 
     public function castBool(bool $bool): mixed
     {
-        return $bool ? 1 : 0;
+        return !$this->bool()
+            ? $bool ? 1 : 0
+            : $bool;
     }
 
     public function castDateTime(DateTimeInterface $dateTimeInterface): mixed
@@ -1003,7 +1010,9 @@ class SQLDialect extends DialectAbstract
 
     public function parseBool(mixed $value): bool
     {
-        return $value == 1 ? true : false;
+        return !$this->bool()
+            ? $value == 1 ? true : false
+            : $value;
     }
 
     public function parseDateTime(string $string): ?DateTime
@@ -1019,6 +1028,11 @@ class SQLDialect extends DialectAbstract
         } catch (Throwable $exception) {
             return null;
         }
+    }
+
+    public function bool(): bool
+    {
+        return static::BOOL;
     }
 
     public function generatedByDefaultAsIdentity(): bool
