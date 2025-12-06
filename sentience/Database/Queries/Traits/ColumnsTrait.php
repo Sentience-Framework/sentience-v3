c916ebeb960849f5950d0aff6953a1ad5c7a6537
UniForceMusic <51342446+UniForceMusic@users.noreply.github.com>
2025-12-06 22:58:42 +0100

    Make 32 bits default for identity columns

--- sentience/Database/Queries/Traits/ColumnsTrait.php
+++ sentience/Database/Queries/Traits/ColumnsTrait.php
@@ -2,36 +2,13 @@
 
 namespace Sentience\Database\Queries\Traits;
 
-use Sentience\Database\Exceptions\QueryException;
-use Sentience\Database\Queries\Objects\Alias;
-use Sentience\Database\Queries\Objects\Raw;
-use Sentience\Database\Queries\Query;
-
 trait ColumnsTrait
 {
     protected array $columns = [];
 
     public function columns(array $columns): static
     {
-        $this->columns = array_map(
-            function (string|array|Alias|Raw $value, string $key): string|array|Alias|Raw {
-                if (ctype_digit($key)) {
-                    return $value;
-                }
-
-                if ($value instanceof Alias) {
-                    if ($value->alias != $key) {
-                        throw new QueryException('alias syntax should not be used with alias object');
-                    }
-
-                    return $value;
-                }
-
-                return Query::alias($value, $key);
-            },
-            $columns,
-            array_keys($columns)
-        );
+        $this->columns = $columns;
 
         return $this;
     }
