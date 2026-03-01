294df97e3a2cf74c36f2b30d9c525a15abee81ed
UniForceMusic <51342446+UniForceMusic@users.noreply.github.com>
2026-03-02 00:36:06 +0100

    Fix select query in alias

--- sentience/Database/Queries/Objects/Alias.php
+++ sentience/Database/Queries/Objects/Alias.php
@@ -3,12 +3,11 @@
 namespace Sentience\Database\Queries\Objects;
 
 use Sentience\Database\Queries\Interfaces\Sql;
-use Sentience\Database\Queries\SelectQuery;
 
 class Alias
 {
     public function __construct(
-        public string|array|SelectQuery|Sql $identifier,
+        public string|array|Sql $identifier,
         public string $alias
     ) {
     }
