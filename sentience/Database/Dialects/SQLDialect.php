2624ea0d253db144bdae6f76253913dc399aa0fc
UniForceMusic <51342446+UniForceMusic@users.noreply.github.com>
2026-09-10 22:52:43 +0200

    Lateral is true by default

--- sentience/Database/Dialects/SQLDialect.php
+++ sentience/Database/Dialects/SQLDialect.php
@@ -46,7 +46,7 @@ class SQLDialect extends DialectAbstract
     public const bool DROP_INDEX_ON_TABLE = false;
     public const bool GENERATED_BY_DEFAULT_AS_IDENTITY = true;
     public const bool INDEX_EXISTS = false;
-    public const bool LATERAL = false;
+    public const bool LATERAL = true;
     public const bool ON_CONFLICT = false;
     public const bool RETURNING = false;
     public const bool SAVEPOINTS = true;
